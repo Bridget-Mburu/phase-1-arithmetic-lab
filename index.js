55b9c4fe033966b0cbe6c8de4162e71f3e3357af
bridget <bridget.mburu.w@gmail.com>
2024-07-02 23:56:13 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 1
+let num2 = 62
+const multiply = (num1 * num2)
 
-// Write your code here
+const random = 42
+
+let num3 = 100
+let num4 = 12
+const mod = (num3 % num4)
+
+Math.max=(15, 17, 20)
+const max = 20
